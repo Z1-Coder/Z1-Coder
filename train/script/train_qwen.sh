9e7b1f6912fd701310a1f439a31b66dbf18b94d2
Zhaojian Yu <57215365+answers111@users.noreply.github.com>
2025-02-10 19:28:37 +0800

    Update train_qwen.sh

--- train/script/train_qwen.sh
+++ train/script/train_qwen.sh
@@ -11,18 +11,18 @@ torchrun --nproc_per_node=8 \
     --master_addr $SATURN_JOB_LEADER \
     --master_port=20011 \
     train.py \
-    --model_name_or_path Qwen/Qwen2.5-32B-Instruct \
+    --model_name_or_path Qwen/Qwen2.5-7B-Instruct \
     --data_path /home/jovyan/workspace/Z1-Coder/data/qwen.json \
     --bf16 True \
     --tf32 True \
     --output_dir $SAVE_PATH \
-    --num_train_epochs 3 \
+    --num_train_epochs 2 \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 60 \
+    --save_steps 100 \
     --save_total_limit 17 \
     --learning_rate 1e-5 \
     --weight_decay 0. \
