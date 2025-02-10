30555c00d062fff9a4b8f22c141ced32a1257f14
Zhaojian Yu <57215365+answers111@users.noreply.github.com>
2025-02-10 20:38:52 +0800

    Update download.py

--- train/download.py
+++ train/download.py
@@ -1,5 +1,5 @@
 from transformers import AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-Coder-7B-Instruct',cache_dir='/home/jovyan/shared/yilunz/z1')
+model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-Coder-7B-Instruct')
 
 print(model)
